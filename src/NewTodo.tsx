aabd6f378df8b5100919ed54a6265cb53adad12b
p9001 <david.omokafe@fidelitybank.ng>
2022-11-23 23:57:53 +0100

    typescript core attribute

--- src/NewTodo.tsx
+++ src/NewTodo.tsx
@@ -5,7 +5,8 @@ const inputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler =(event:React.FormEvent)=>{
     event.preventDefault();
-
+    // const enteredTodo = inputRef.current?.value //the ? signifies a possible null value at firs
+    const enteredTodo = inputRef.current!.value // the ! signifies to ts you are sure there will be a value at this point
   }
 
  return( 
